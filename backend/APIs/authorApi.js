fa37705053a3cb803a94250f8d4d7b88db257f8b
SandaAdharsh <adharshsanda@gmail.com>
2024-06-11 10:47:05 +0530

    Moved git from backend to root

---
+++ backend/APIs/authorApi.js
@@ -0,0 +1,107 @@
+const exp=require('express');
+const authorApp=exp.Router();
+const expressAsyncHandler=require('express-async-handler')
+const verifyToken=require('../Middlewares/verifyToken');
+//get authorsCollection from server.js
+authorApp.use((req,res,next)=>{
+    authorsCollection=req.app.get('authorsCollection');
+    articlesCollection=req.app.get('articlesCollection');
+    next();
+})
+//import bcrypts
+const bcryptjs=require('bcryptjs');
+//import jsonwebtoken
+const jwt=require('jsonwebtoken');
+
+//request handler for author Registration
+authorApp.post('/author',expressAsyncHandler(async(req,res)=>{
+    const newUser=req.body;
+    //comparing it with username
+    const dbUser=await authorsCollection.findOne({username:newUser.username});
+    if(dbUser!=null){
+     res.send({message:"Author already registered"});
+    }
+    else{
+     //hash the password
+     const hashedPassword=await bcryptjs.hash(newUser.password,6);
+     newUser.password=hashedPassword;
+     //add it to the collection
+     await authorsCollection.insertOne(newUser);
+     res.send({message:"author registered"});
+ 
+    }
+ }))
+require('dotenv').config();
+//request handler for author Login
+authorApp.post('/login',expressAsyncHandler(async(req,res)=>{
+    //Get the user credentials
+    const userCred=req.body;
+    //compare or find user with the username
+    const dbUser=await authorsCollection.findOne({username:userCred.username});
+   
+    //if dbuser is null
+    if(dbUser===null){
+        res.send({message:"user does not exist"})
+    }
+    else{
+        //compare the hashed password
+        const status=await bcryptjs.compare(userCred.password,dbUser.password);
+        if(status===false){
+            res.send({message:"Incorrect password"});
+        }
+        else{
+            //create json web token
+            const signedToken=jwt.sign({username:dbUser.username},process.env.SECRET_KEY,{expiresIn:20});
+            //send the response
+            res.send({message:"login success",token:signedToken,user:dbUser});
+
+        }
+    }
+}))
+
+//adding new article by author
+authorApp.post('/article',verifyToken,(async(req,res)=>{
+    //get new article
+    
+    const newArticle=req.body;
+    //posting to articles collection
+    await articlesCollection.insertOne(newArticle);
+    //send response
+    res.send({message:"New article created"});
+
+    
+}))
+
+//request handler for updating an article by author
+authorApp.put('/article',verifyToken,expressAsyncHandler(async(req,res)=>{
+    //get body from request 
+    const modifiedArticle=req.body;
+    //update article by articleid
+    await articlesCollection.updateOne({articleId:modifiedArticle.articleId},{$set:modifiedArticle});
+    res.send({message:"Article is modified"});
+}))
+
+
+//soft deleting an article by author
+authorApp.put('/article/:articleId',verifyToken,expressAsyncHandler(async(req,res)=>{
+    //get article id
+    const articleFromUrl=req.params.articleId;
+    //get article body
+    const articleToDelete=req.body;
+    //updating article by changing its status to false
+    await articlesCollection.updateOne({articleId:articleFromUrl},{$set:{...articleToDelete,status:false}})
+    res.send({message:"Article is removed"})
+}))
+
+
+//viewing articles of his own by author by his name
+authorApp.get('/articles/:username',verifyToken,expressAsyncHandler(async(req,res)=>{
+    //get authorname from url
+    const authorName=req.params.username;
+    //get articles of author whose status is true
+    const articlesList=await articlesCollection.find({status:true}).toArray();
+   //send the response
+   res.send({message:"List of articles",payload:articlesList}); 
+}))
+
+module.exports=authorApp;
---
+++ backend/APIs/userApi.js
@@ -0,0 +1,94 @@
+const exp=require('express');
+//importing miniExpress
+const userApp=exp.Router();
+//importing expressAsyncHandler 
+const expressAsyncHandler=require('express-async-handler')
+const verifyToken=require('../Middlewares/verifyToken')
+//testing the route
+userApp.get('/test-user',(req,res)=>{
+    res.send({message:"This message is from user api"});
+})
+//import jsonwebtoken
+const jwt=require('jsonwebtoken');
+//import bcryptjs
+const bcryptjs=require('bcryptjs');
+
+//get usersCollection from server.js to add the data
+userApp.use((req,res,next)=>{
+    usersCollection=req.app.get('usersCollection');
+    articlesCollection=req.app.get('articlesCollection');
+    next();
+})
+require('dotenv').config();
+
+//request handler for userRegistration route
+//expressAsyncHandler will handle the async errors without crashing the server
+userApp.post('/user',expressAsyncHandler(async(req,res)=>{
+    //Get the body 
+    const newUser=req.body;
+    //check for duplicate user with same username
+    const dbUser=await usersCollection.findOne({username:newUser.username});
+    //if their is a user with same username
+    if(dbUser!=null){
+        res.send({message:"user already existed"});
+    }
+    else{
+        //hash the password
+        const hashedPassword=await bcryptjs.hash(newUser.password,7);
+        //change the password to hashed password
+        newUser.password=hashedPassword;
+        //Insert the data to usersCollection
+        await usersCollection.insertOne(newUser);
+        //send the response
+        res.send({message:"user created"});
+    }
+}))
+
+
+//request handler for user login route
+userApp.post('/login',expressAsyncHandler(async(req,res)=>{
+    //Get the user credentials
+    const userCred=req.body;
+    //compare or find user with the username
+    const dbUser=await usersCollection.findOne({username:userCred.username});
+    //if dbuser is null
+    if(dbUser===null){
+        res.send({message:"user does not exist"})
+    }
+    else{
+        //compare the hashed password
+        const status=await bcryptjs.compare(userCred.password,dbUser.password);
+        if(status===false){
+            res.send({message:"Incorrect password"});
+        }
+        else{
+            //create json web token
+            const signedToken=jwt.sign({username:dbUser.username},process.env.SECRET_KEY,{expiresIn:20});
+            //send the response
+            res.send({message:"login success",token:signedToken,user:dbUser});
+
+        }
+    }
+}))
+
+
+//request handler for viewing all articles by user
+userApp.get('/articles',verifyToken,expressAsyncHandler(async(req,res)=>{
+    const articlesCollection=req.app.get('articlesCollection');
+    //to get all articles
+    const articlesList=await articlesCollection.find().toArray();
+    //send res
+    res.send({message:"Articles",payload:articlesList});
+}))
+
+//request handler for writing comments
+userApp.post('/comment/:articleId',verifyToken,expressAsyncHandler(async(req,res)=>{
+    //get comment
+    const userComment=req.body;
+    const articleIdFromUrl=req.params.articleId;
+    //add comment to article of comments array
+    await articlesCollection.updateOne({articleId:articleIdFromUrl},{$addToSet:{comments:userComment}})
+    res.send({message:"comment posted"})
+}))
+//exporting the userApp
+module.exports=userApp;
